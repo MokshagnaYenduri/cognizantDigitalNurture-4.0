86d5ef27b85de409054b63cfd8fd064659acfc51
Mokshagna <mokshagnayenduri@gmail.com>
2025-06-29 16:05:50 +0530

    add necessary files to run verifying interactions

---
+++ week-2/mockitoExercises/verifyingInteractions/code/MyServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.exe.JUnit5Setup;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}

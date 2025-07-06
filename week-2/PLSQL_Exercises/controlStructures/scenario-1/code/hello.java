b62a3d215f51bfe0059716eef48699473c40c3ec
Mokshagna <mokshagnayenduri@gmail.com>
2025-07-06 14:49:08 +0530

    add maven project output

---
+++ week-2/PLSQL_Exercises/controlStructures/scenario-1/code/hello.java
@@ -0,0 +1,12 @@
+public class hello {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		System.out.println(args.length);
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
+ 

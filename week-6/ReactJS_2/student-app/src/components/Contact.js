02fa5753ced2f35ac26ace0818e840f233bb64f6
Mokshagna <mokshagnayenduri@gmail.com>
2025-07-27 14:04:40 +0530

    update the logic in contact.js

---
+++ week-6/ReactJS_2/student-app/src/components/Contact.js
@@ -0,0 +1,11 @@
+import react, {Component} from "react";
+import class About extends Component {
+    render(){
+        return(
+            <div>
+                <h3>This is the contact page of the student management portal.</h3>
+            </div>
+        )
+    }
+}
+export default About;

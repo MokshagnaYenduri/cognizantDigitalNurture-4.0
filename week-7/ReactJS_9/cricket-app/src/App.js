ad6aa3987841aba047c937ac983f89f868e2ea1f
Mokshagna <mokshagnayenduri@gmail.com>
2025-08-03 09:40:55 +0530

    update app.js

--- week-7/ReactJS_9/cricket-app/src/App.js
+++ week-7/ReactJS_9/cricket-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ListofPlayers from './listOfPlayers';
+import IndianPlayers from './indianPlayers';
 
 function App() {
+  const flag = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cricket App</h1>
+      {flag ? <ListofPlayers /> : <IndianPlayers />}
     </div>
   );
 }
